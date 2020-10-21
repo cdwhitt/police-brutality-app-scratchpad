4a8cd9e36885f4e506f4f5068382da1a10c76bd7
cdwhitt <cwhitt91@gmail.com>
2020-10-21 18:42:12 -0400

    Build basic search functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import _ from "lodash"
+import "./App.css"
+import { Segment, Dropdown, Button, Card, List, Label } from "semantic-ui-react"
+import axios from "axios"
+import * as statesJSON from "./states.json"
+const states = Object.values(statesJSON.default)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const stateOptions = _.map(states, (state, index) => ({
+	key: states[index],
+	text: state,
+	value: states[index],
+}))
+
+const App = () => {
+	const [stateQuery, setStateQuery] = useState("")
+	const [results, setResults] = useState([])
+	const fetchStateData = async () => {
+		const { data } = await axios.get(
+			`https://api.846policebrutality.com/api/incidents?&filter[state]=${stateQuery}`
+		)
+
+		setResults(data.data)
+	}
+
+	const fetchByTag = async (tag) => {
+		const { data } = await axios.get(
+			`https://api.846policebrutality.com/api/incidents?filter[tags]=${tag}`
+		)
+
+		setResults(data.data)
+	}
+
+	const incidentCards = results.map(
+		({
+			city,
+			data,
+			date,
+			description,
+			geocoding,
+			id,
+			links,
+			pb_id,
+			state,
+			tags,
+			title,
+		}) => (
+			<Card>
+				<Card.Content>
+					<Card.Header>{title}</Card.Header>
+					<Card.Meta>Location: {city}</Card.Meta>
+					<Card.Description>
+						Links:
+						{links.map((link) => (
+							<List>
+								<List.Item>
+									<a href={link}>{link.substring(0, 30)}...</a>
+								</List.Item>
+							</List>
+						))}
+						Tags:
+						{tags.map((tag) => (
+							<List>
+								<List.Item>
+									<Label as="a" onClick={() => fetchByTag(tag)}>
+										#{tag}
+									</Label>
+								</List.Item>
+							</List>
+						))}
+					</Card.Description>
+				</Card.Content>
+			</Card>
+		)
+	)
+
+	//city: //String
+	//data: //??
+	//date: //Date String
+	//description: //String?
+	//geocoding: // Object w/ lat: & long:
+	//id: //String
+	//links: //Array of strings
+	//pb_id: //String
+	//state: //String
+	//tags: //Array of strings
+	//title: //String
+
+	return (
+		<>
+			<Segment>
+				<Dropdown
+					value={stateQuery}
+					placeholder="State"
+					search
+					selection
+					options={stateOptions}
+					onChange={(event) => setStateQuery(event.target.textContent)}
+				/>
+				<Button icon="search" color="green" onClick={fetchStateData}></Button>
+			</Segment>
+			<Card.Group>{incidentCards}</Card.Group>
+		</>
+	)
 }
 
-export default App;
+export default App
